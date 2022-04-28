d61f670e679c344d79bba486d817c25556feb4c6
wanglebin <wanglebin@joyy.com>
2022-04-28 14:29:44 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
 function App() {
+  const getRequest = async () => {
+    await axios({
+      method: 'get',
+      url: 'https://loserkeys.tk/flow/app/vue/2.5.16/vue.min.js',
+    })
+  }
+
+  const sendRequest = async () => {
+    await axios({
+      method: 'post',
+      url: '/user/12345',
+      data: {
+        firstName: 'Fred',
+        lastName: 'Flintstone',
+      },
+      params: {
+        page: 1,
+        current: '231321',
+      },
+      header: {
+        header_uuid: '321321212-13221321-321321-ewqeq',
+      },
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>clourdflire 测试demo</div>
+      <button onClick={getRequest}>获取 一个js 资源</button>
+      <br />
+      <br />
+      <button onClick={sendRequest}>提交一些请求</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
